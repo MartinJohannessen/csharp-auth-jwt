3fb465896be649b9e7b9ab4a82f6869df902e027
Martin Skivenesvåg Johannessen <martin.johannessen@no.experis.com>
2024-03-05 13:38:05 +0100

    save the 'progress'

---
+++ exercise.wwwapi/Data/DataContext.cs
@@ -0,0 +1,30 @@
+﻿using exercise.wwwapi.Data;
+using exercise.wwwapi.Models.post;
+using exercise.wwwapi.Models.user;
+using Microsoft.EntityFrameworkCore;
+using System.Net.Sockets;
+
+namespace exercise.wwwapi.Data
+{
+    public class DataContext : DbContext
+    {
+        private string _connectionString;
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            _connectionString = configuration.GetValue<string>("ConnectionStrings:DefaultConnection")!;
+            this.Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(_connectionString);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Post> Posts { get; set; }
+    }
+}
---
+++ exercise.wwwapi/Models/post/Post.cs
@@ -0,0 +1,12 @@
+﻿using exercise.wwwapi.Models.user;
+
+namespace exercise.wwwapi.Models.post
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public string UserId { get; set; }
+        public User User { get; set; } = null!;
+    }
+}
